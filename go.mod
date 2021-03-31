8d60fa321779d91456340904de44fbbda58bd647
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2021-03-30 18:12:25 -0700

    Basic chat server setup

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/conrunyan/group-dice-roller
 
 go 1.15
+
+require (
+	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/olahol/melody v0.0.0-20180227134253-7bd65910e5ab // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
+)
+
+const IndexFile = "./public/index.html"
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		log.Println(string(msg))
+		m.Broadcast(msg)
+	})
+
+	r.Run("127.0.0.1:8080")
+}
